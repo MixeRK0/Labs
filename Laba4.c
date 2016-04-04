2d1294cc97423b99936c01ca0e8ba2b06ccbcf5c
MixeRK0 <arhangelskinikolay@gmail.com>
2016-04-04 16:07:30 +0300

    Update Laba4.c

--- Laba4.c
+++ Laba4.c
@@ -5,11 +5,11 @@ void main()
  {
   int n,a=0,i,b;
   clrscr();
-  printf("Ввод N=");
+  printf("Enter value N=");
   scanf("%i",&n);
   for (i=1;i<=n;i++) a=a+pow(i,3);
-  printf("\n Результат работы программы а=%i",a);
+  printf("\n Rusult of programm а=%i",a);
   b=pow(n,2)*pow(n+1,2)/4;
-  printf("\n Контрольное значение=%i",b);
+  printf("\n Control value=%i",b);
   getch();
  }
